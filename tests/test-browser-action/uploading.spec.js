74987b4778047e22dafb9bbd789bed273648d41a
PleD1k <pledik.moy@gmail.com>
2025-06-23 09:13:59 +0300

    Comments fix

--- tests/test-browser-action/uploading.spec.js
+++ tests/test-browser-action/uploading.spec.js
@@ -3,9 +3,9 @@ const { test, expect } = require('@playwright/test');
 test.describe('Task6', () => {
   test('Check uploading txt file', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/upload');
-    await page.setInputFiles('#file-upload', 'tests/test-browser-action/test-data/sample.txt');
+    await page.setInputFiles('#file-upload', 'tests/test-browser-action/test-data/test.txt');
     await page.click('#file-submit');
 
-    await expect(page.locator('#uploaded-files')).toHaveText('sample.txt');
+    await expect(page.locator('#uploaded-files')).toHaveText('test.txt');
   });
 });
